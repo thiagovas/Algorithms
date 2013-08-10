b1aa9b4d089ff7cc0faf324f656ce00129d5008a
Thiago Vieira <thiagovieiraas@gmail.com>
2013-08-10 01:55:56 -0300

    Update Dijkstra.cpp
    Alterei o algoritmo de Dijkstra, agora está um pouco melhor que antes.

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,51 +3,51 @@
      Algoritmo de Dijkstra, ele calcula a menor distância entre dois pontos.
 */
 #include <iostream>
-#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
 #include <queue>
+#include <algorithm>
+#include <utility>
 #include <functional>
 using namespace std;
+
+#define INF 2100000000
+
+typedef pair<int, int> PII;
+
 
 /*
     O parâmetro grafo é uma lista de adjacência. Para cada nó, tenho um vector de pairs, cada pair representa uma aresta,
     onde o primeiro elemento do pair é o peso da aresta e o segundo elemento é o nó conectado.
     O retorno da função é o custo mínimo para sair do nó start para chegar em end.
 */
-long long int dijkstra(vector<vector<pair<int, int> > > *grafo, int start, int end)
+int Dijkstra(vector<vector<PII > > *grafo, int start, int end)
 {
-    priority_queue<pair<int, long long int>, vector<pair<int, long long int> >, greater<pair<int, long long int> > > q;
-    pair<int, int> atual;
-    long long int *pesos = (long long int*)calloc(grafo->size(), sizeof(long long int));
-    long long int retorno = -1, soma;
+    priority_queue<PII, vector<PII >, greater<PII > > nodes;
+    PII atual;
+    vector<int> pesos(grafo->size(), INF);
     
-    for(unsigned int i = 0; i < grafo->size(); i++)
-        pesos[i] = -1;
     pesos[start] = 0;
-    q.push(make_pair(0, start));
+    nodes.push(make_pair(0, start));
     
-    while(!q.empty())
+    while(!nodes.empty())
     {
-        atual = q.top();
-        q.pop();
+        atual = nodes.top();
+        nodes.pop();
         
-        if(atual.second == end)
-        {
-            retorno = pesos[atual.second];
-            break;
-        }
-        soma = pesos[atual.second];
+        if(atual.second == end) break;
         
-        for(vector<pair<int, int> >::iterator i = (*grafo)[atual.second].begin(); i != (*grafo)[atual.second].end(); i++)
-        {
-            if(pesos[i->second] == -1) q.push(make_pair(soma+(i->first), i->second));
-            
-            if(((pesos[i->second] > soma+(i->first)) && (pesos[i->second] != 0)) || pesos[i->second] == -1)
-                pesos[i->second] = soma+(i->first);
-        }
+        for(vector<PII>::iterator it = (*grafo)[atual.second].begin(); it != (*grafo)[atual.second].end(); it++)
+            if(pesos[it->second] == -1 || pesos[it->second] > it->first+pesos[atual.second])
+            {
+                pesos[it->second] = it->first+pesos[atual.second];
+                nodes.push(make_pair(pesos[it->second], it->second));
+            }
     }
-    free(pesos);
-    return retorno;
+    
+    if(pesos[end] != INF)
+        return pesos[end];
+    
+    return -1;
 }
